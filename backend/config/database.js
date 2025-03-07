831bd20a3278b53c202feab99be401d6178eb005
Arya Jadhav <119299569+arya1917@users.noreply.github.com>
2025-03-07 15:12:37 +0800

    DATABASE Configuration

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-
+// Database connection configuration using environment variables
 const sequelize = new Sequelize(process.env.loan_forecasting, process.env.loan_user, process.env.password, {
-    host: process.env.localhost,
-    port: process.env.PORT,//+
+    host: process.env.DB_HOST, // Use DB_HOST instead of localhost
+    port: process.env.DB_PORT || 5432, // Default to 5432 if DB_PORT is not defined
     dialect: 'postgres',
     logging: false
 });
